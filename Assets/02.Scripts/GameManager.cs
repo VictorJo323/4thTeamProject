2473b4ccfb6221d377bb5108cd68102589635bd6
FrostKim0208 <admiral0208@naver.com>
2023-12-15 14:30:13 +0900

    Add : New Scripts - GameManager,AudioManager

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+                _instance = go.GetComponent<GameManager>();
+                DontDestroyOnLoad(go);
+            }
+            return _instance;
+        }
+        set
+        {
+            if (_instance == null) _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        //게임매니저가 없다면 현재 오브젝트를 게임매니저로
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        //게임매니저가 2개이상 스크립트로 들어가 있을시 나중에 지정받은 오브젝트 파괴
+        else
+        {
+            if (_instance != this) Destroy(this);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
