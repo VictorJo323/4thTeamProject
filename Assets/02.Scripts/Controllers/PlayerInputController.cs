b95c4ffc0cb57c2f118bfa7114d0769879388750
Yanginho <didvktjstod@naver.com>
2023-12-18 11:27:41 +0900

    [ADD]Character

---
+++ Assets/02.Scripts/Controllers/PlayerInputController.cs
@@ -0,0 +1,42 @@
+using Cainos.PixelArtTopDown_Basic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Player : TopDownCharacterController
+{
+    // Start is called before the first frame update
+    private Camera _camera;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        //Debug.Log("OnMove"+value.ToString());
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+    public void OnLook(InputValue value)
+    {
+        //Debug.Log("OnLook" + value.ToString());
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+    public void OnFire(InputValue value)
+    {
+        //Debug.Log("OnFire" + value.ToString());
+        IsAttacking = value.isPressed;
+    }
+}
---
+++ Assets/02.Scripts/Controllers/TopDownAnimationController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class TopDownAnimationController : TopDownAnimations
+{
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int IsHit = Animator.StringToHash("IsHit");
+
+    private HealthSystem _healthSystem;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _healthSystem = GetComponent<HealthSystem>();
+    }
+    void Start()
+    {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += Move;
+
+        if (_healthSystem != null)
+        {
+            _healthSystem.OnDamage += Hit;
+            _healthSystem.OnInvincibilityEnd += InvincibilityEnd;
+        }
+    }
+
+    private void Attacking(AttackSO sO)
+    {
+        animator.SetTrigger(Attack);
+    }
+
+    private void Move(Vector2 obj)
+    {
+        animator.SetBool(IsWalking, obj.magnitude > .5f);
+    }
+
+    private void Hit()
+    {
+        animator.SetBool(IsHit, true);
+    }
+
+    private void InvincibilityEnd()
+    {
+        animator.SetBool(IsHit, false);
+    }
+
+
+
+
+
+    void Update()
+    {
+
+    }
+}
