e89c55c90a3f872cee03620eba94df7fa2ed730a
chamgilom <149364808+chamgilom@users.noreply.github.com>
2023-12-19 19:29:29 +0900

    [ADD]EnemyMovement

---
+++ Assets/02.Scripts/Controller/EnemyHpController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHpController : HealthSystem
+    [SerializeField] private CanvasGroup healthGroup
+    // Start is called before the first frame update
+    public override 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
