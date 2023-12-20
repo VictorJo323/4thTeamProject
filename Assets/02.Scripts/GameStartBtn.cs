55303e83113f899fb9baffb5bc8bc502d783d6cf
FrostKim0208 <admiral0208@naver.com>
2023-12-20 20:48:58 +0900

    [Mod] GameStartBtn.cs

--- Assets/02.Scripts/GameStartBtn.cs
+++ Assets/02.Scripts/GameStartBtn.cs
@@ -7,7 +7,7 @@ public class GameStartBtn : MonoBehaviour
 {
     public void GameStart()
     {
-        //TODO 공백 부분에 씬이름 적어주세요.
-        SceneManager.LoadScene("");
+        
+        SceneManager.LoadScene("03.Stage1");
     }
 }
